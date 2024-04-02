50b1da47ca82547b5f731bdb3186567ac7b8b2cb
Bhanu <bhanusngh12345@gmail.com>
2024-04-03 00:13:02 +0530

    made some more chnages which conflicts during deployment

--- src/app/(Home)/page.tsx
+++ src/app/(Home)/page.tsx
@@ -1,5 +1,4 @@
 import HeroSection from "@/components/shared/HeroSection";
-import { MyComponent } from "@/components/shared/design";
 import Image from "next/image";
 
 export default function Home() {
